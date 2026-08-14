0d59ccfcce5b4f7b2721b73d0da9fc19bc500cfa
DJAINCOOL <54236855+DJAINCOOL@users.noreply.github.com>
2026-08-14 12:39:21 +0530

    Add main Terraform configuration

---
+++ main.tf
@@ -0,0 +1,34 @@
+module "vpc" {
+  source      = "./modules/vpc"
+  network_name = "vpc-test1"
+  project_id  = var.project_id
+  region      = var.region
+  subnets = [
+    { name = "sub-test1" , cidr = "10.0.1.0/24" },
+    { name = "sub-test2" , cidr = "10.0.2.0/24" }
+  ]
+}
+
+module "compute" {
+  source     = "./modules/compute"
+  project_id = var.project_id
+  instances = [
+    {
+      name       = "gce-sub-test1"
+      zone       = "${var.region}-a"
+      subnetwork = module.vpc.subnets["sub-test1"]
+    },
+    {
+      name       = "gce-sub-test2"
+      zone       = "${var.region}-a"
+      subnetwork = module.vpc.subnets["sub-test2"]
+    }
+  ]
+}
+
+module "storage" {
+  source     = "./modules/storage"
+  project_id = var.project_id
+  bucket_name = "bucket-sub-1"
+  location   = "US"
+}
